161dcee317dd7dea86626e7b06fe084d88a5ef06
carabusu <mihai.bulhac@yahoo.com>
2016-08-04 10:45:47 +0300

    Update test3.js

--- reports/_lastExecution/test3.js
+++ reports/_lastExecution/test3.js
@@ -45,4 +45,17 @@
 		  }
 
 	};
+	$.expr[':'].containsFullText = function(a, i, m) {
+		  textSearch = m[3];
+		  textSearch = textSearch.replace(/\\/g, "");
+		  elemText = $(a).html();
+		  elemText = elemText.replace(/&nbsp;/g, " ");
+		  //console.log("element: " + elemText);
+		  //console.log("text:    " + textSearch);
+		  if (elemText===textSearch) {
+		    //console.log("---" + elemText);
+		    return $(a).text();
+		  }
+
+	};
 
