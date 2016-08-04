bf11aad6b28b701c582faf36b43857c870bbe02f
carabusu <mihai.bulhac@yahoo.com>
2016-08-04 12:11:03 +0300

    Update test3.js

--- reports/_lastExecution/test3.js
+++ reports/_lastExecution/test3.js
@@ -35,7 +35,7 @@
 	$.expr[':'].containsPartialText = function(a, i, m) {
 		  textSearch = m[3];
 		  textSearch = textSearch.replace(/\\/g, "");
-		  elemText = $(a).html();
+		  elemText = $(a).html().trim();
 		  elemText = elemText.replace(/&nbsp;/g, " ");
 		  //console.log("element: " + elemText);
 		  //console.log("text:    " + textSearch);
@@ -47,12 +47,13 @@
 	};
 	$.expr[':'].containsFullText = function(a, i, m) {
 		  textSearch = m[3];
+		  textSearch = textSearch.replace(/\\/g, "");
 		  elemText = $(a).html().trim();
-		  
+		  elemText = elemText.replace(/&nbsp;/g, " ");
 		  console.log("element: " + elemText);
 		  //alert("element: " + elemText + "---" + m[3]);
 		  console.log("text:    " + textSearch);
-		  if (elemText===textSearch) {
+		  if (elemText==textSearch) {
 		    console.log("---" + elemText);
 		    return $(a).text();
 		  }
