2cebc2251690cc3b326d71cfac83e5d3da7c7572
carabusu <mihai.bulhac@yahoo.com>
2016-08-04 18:49:26 +0300

    Update test3.js

--- reports/_lastExecution/test3.js
+++ reports/_lastExecution/test3.js
@@ -1,12 +1,9 @@
-//This selector will find exact text without case sensitivity
-	
-	$.expr[':'].textEquals = function(a, i, m) {  
-		 return $(a).html().trim().match("^" + m[3] + "$");  
-	};
-	
-	$.expr[':'].containsPartialText = function(a, i, m) {
-		  textSearch = m[3];
-		  textSearch = textSearch.replace(/\\/g, "");
+jQuery(function($) {
+    
+	$.extend($.expr[':'],{
+   		containsPartialText:  function(a, i, m) {
+        textSearch = m[3];
+		textSearch = textSearch.replace(/\\/g, "");
 		  elemText = $(a).html().trim();
 		  elemText = elemText.replace(/&nbsp;/g, " ");
 		  console.log("element: " + elemText);
@@ -15,18 +12,23 @@
 		    //console.log("---" + elemText);
 		    return $(a).text();
 		  }
-
-	};
+    }
+	});
 	
-	$.expr[':'].mazga= function(a, i, m) {
-		   textSearch = m[3];
-		  textSearch = textSearch.replace(/\\/g, "");
-		  elemText = $(a).html().trim();
-		  elemText = elemText.replace(/&nbsp;/g, " ");
-		  console.log("element: " + elemText);
-		  //console.log("text:    " + textSearch);
-		  if (elemText.includes(textSearch)) {
-		    //console.log("---" + elemText);
-		    return $(a).text();
-		  }
-	};
+	
+	$.extend($.expr[':'],{
+   		containsFullText:  function(a, i, m) {
+	          textSearch = m[3];
+			  textSearch = textSearch.replace(/\\/g, "");
+			  elemText = $(a).html().trim();
+			  elemText = elemText.replace(/&nbsp;/g, " ");
+			  console.log("element: " + elemText);
+			  //console.log("text:    " + textSearch);
+			  if (elemText===textSearch) {
+			    //console.log("---" + elemText);
+			    return $(a).text();
+		  	}
+    }
+	});
+	
+});
