b51529b66e9cdaab2dadc347eb404a50319922b8
carabusu <mihai.bulhac@yahoo.com>
2016-10-12 14:49:45 +0300

    Update test3.js

--- reports/_lastExecution/test3.js
+++ reports/_lastExecution/test3.js
@@ -32,50 +32,60 @@ jQuery(function($) {
     }
 	});
 	
-	
-jQuery.fn.extend({
-    getMandatoryFields: function (a,i,m) {
-		var returnStepsCode="";
-		$(this).each(function (index, value){
-			
-			var splitStr = $(this).text().split(' ');
-			for (var i = 0; i < splitStr.length; i++) {
-			   splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);     
-		   }
-		   var newLabelValue=splitStr.join(' ').trim().replace(/[^a-zA-Z]+/g, ''); 
-		   //console.log(">" + newLabelValue);
-		   
-		   //console.log($(this).parent().find($("[role*='combobox']:visible")).length);
-		   if ($(this).parent().find($("[role*='combobox']:visible")).length>0){
-			   returnStepsCode=returnStepsCode + ".combo(\"myValue\", Fields." + newLabelValue + ")\n";
-			   //console.log(".combo(\"myValue\", Fields." + newLabelValue + ")" );
-		   }
-		   if ($(this).parent().find($(" input:visible:visible")).length>0){
-			   var attrElementID=$(this).parent().find($(" input:visible:visible")).attr("id");
-			   if(attrElementID.includes('DateTimePicker')){
-				   returnStepsCode=returnStepsCode + ".input(DateTime.addToCurrentDate(noOfDays), Fields." + newLabelValue + ")\n";
-			   	   //console.log(".input(DateTime.addToCurrentDate(noOfDays), Fields." + newLabelValue + ")");
+
+	jQuery.fn.extend({
+	    getMandatoryFields: function (a,i,m) {
+			var returnStepsCodeShort=".step()\n";
+			var returnStepsCodeLong="";
+			$(this).each(function (index, value){
+
+				var splitStr = $(this).text().split(' ');
+				for (var i = 0; i < splitStr.length; i++) {
+				   splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);     
 			   }
-			   if(attrElementID.includes('BluePagesLookupField')){
-				   returnStepsCode=returnStepsCode + ".input(DateTime.addToCurrentDate(noOfDays), Fields." + newLabelValue + ")\n";
-			   	   //console.log(".input(\"BULHAC, MIHAI (Mihai)\", Fields." + newLabelValue + ")");
+			   var newLabelValue=splitStr.join(' ').trim().replace(/[^a-zA-Z^0-9.]+/g, ''); 
+			   //console.log(">" + newLabelValue);
+
+			   //console.log($(this).parent().find($("[role*='combobox']:visible")).length);
+			   if ($(this).parent().find($("[role*='combobox']:visible")).length>0){
+				   returnStepsCodeShort=returnStepsCodeShort + "	.combo(\"myValue\", Fields." + newLabelValue + ")\n";
+				   returnStepsCodeLong=returnStepsCodeLong + "pgmp.createRequest.combo.selectValue(selectValue, Fields." + newLabelValue + ".val()); \n"
+				   //console.log(".combo(\"myValue\", Fields." + newLabelValue + ")" );
 			   }
-			   if(attrElementID.includes('InputField')){
-				   returnStepsCode=returnStepsCode + ".input(\"test\", Fields." + newLabelValue + ")\n";
-			   	   //console.log(".input(\"test\", Fields." + newLabelValue + ")");
+			   if ($(this).parent().find($(" input:visible:visible")).length>0){
+				   var attrElementID=$(this).parent().find($(" input:visible:visible")).attr("id");
+				   if(attrElementID.includes('DateTimePicker')){
+					   returnStepsCodeShort=returnStepsCodeShort + "	.input(DateTime.addToCurrentDate(noOfDays), Fields." + newLabelValue + ")\n";
+					   returnStepsCodeLong=returnStepsCodeLong + "pgmp.createRequest.input.selectDate(DateTime.addToCurrentDate(noOfDays), Fields." + newLabelValue + ".val()); \n"
+					   //console.log(".input(DateTime.addToCurrentDate(noOfDays), Fields." + newLabelValue + ")");
+				   }
+				   if(attrElementID.includes('BluePagesLookupField')){
+					   returnStepsCodeShort=returnStepsCodeShort + "	.input(\"BULHAC, MIHAI (Mihai)\, Fields." + newLabelValue + ")\n";
+					   returnStepsCodeLong=returnStepsCodeLong + "pgmp.createRequest.input.selectBluePageName(\"BULHAC, MIHAI (Mihai)\", Fields." + newLabelValue + ".val()); \n"
+					   //console.log(".input(\"BULHAC, MIHAI (Mihai)\", Fields." + newLabelValue + ")");
+				   }
+				   if(attrElementID.includes('InputField')){
+					   returnStepsCodeShort=returnStepsCodeShort + "	.input(\"test\", Fields." + newLabelValue + ")\n";
+					   returnStepsCodeLong=returnStepsCodeLong + "pgmp.createRequest.input.type(\"test\", Fields." + newLabelValue + ".val()); \n"
+					   //console.log(".input(\"test\", Fields." + newLabelValue + ")");
+				   }
 			   }
-		   }
-		   if ($(this).parent().find($(" textarea:visible")).length>0){
-			   returnStepsCode=returnStepsCode + ".textArea(\"test\", Fields." + newLabelValue + ")\n";
-			   //console.log(".textArea(\"test\", Fields." + newLabelValue + ")" );
-		   }
-		   if ($(this).parent().find($(" select:visible")).length>0){
-			   returnStepsCode=returnStepsCode + ".multiSelect(multiSelectValue, Fields." + newLabelValue + ")\n";
-			   //console.log(".multiSelect(multiSelectValue, Fields." + newLabelValue + ")" );
-		   }
-		});
-		return  returnStepsCode;
-    }
-});
+			   if ($(this).parent().find($(" textarea:visible")).length>0){
+				   returnStepsCodeShort=returnStepsCodeShort + "	.textArea(\"test\", Fields." + newLabelValue + ")\n";
+				   returnStepsCodeLong=returnStepsCodeLong + "pgmp.createRequest.textArea.type(\"test\", Fields." + newLabelValue + ".val()); \n"
+				   //console.log(".textArea(\"test\", Fields." + newLabelValue + ")" );
+			   }
+			   if ($(this).parent().find($(" select:visible")).length>0){
+				   returnStepsCodeShort=returnStepsCodeShort + "	.multiSelect(multiSelectValue, Fields." + newLabelValue + ")\n";
+				   returnStepsCodeLong=returnStepsCodeLong + "pgmp.createRequest.combo.selectValue(comboValue, Fields." + newLabelValue + ".val()); \n"
+				   //console.log(".multiSelect(multiSelectValue, Fields." + newLabelValue + ")" );
+			   }
+			   returnStepsCodeShort=returnStepsCodeShort+ "	.submit(\"Submit\",submitValue);\n"
+			});
+
+			return  returnStepsCodeShort + "\n\n\n" + returnStepsCodeLong;
+
+	    }
+	});
 	
 });
