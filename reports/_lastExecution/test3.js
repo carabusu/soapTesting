0f00c59fe59908ced9e49caf44dc5ab51da806e7
carabusu <mihai.bulhac@yahoo.com>
2016-10-12 13:28:31 +0300

    Update test3.js

--- reports/_lastExecution/test3.js
+++ reports/_lastExecution/test3.js
@@ -32,4 +32,50 @@ jQuery(function($) {
     }
 	});
 	
+	
+jQuery.fn.extend({
+    getMandatoryFields: function (a,i,m) {
+		var returnStepsCode="";
+		$(this).each(function (index, value){
+			
+			var splitStr = $(this).text().split(' ');
+			for (var i = 0; i < splitStr.length; i++) {
+			   splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);     
+		   }
+		   var newLabelValue=splitStr.join(' ').trim().replace(/[^a-zA-Z]+/g, ''); 
+		   //console.log(">" + newLabelValue);
+		   
+		   //console.log($(this).parent().find($("[role*='combobox']:visible")).length);
+		   if ($(this).parent().find($("[role*='combobox']:visible")).length>0){
+			   returnStepsCode=returnStepsCode + ".combo(\"myValue\", Fields." + newLabelValue + ")\n";
+			   //console.log(".combo(\"myValue\", Fields." + newLabelValue + ")" );
+		   }
+		   if ($(this).parent().find($(" input:visible:visible")).length>0){
+			   var attrElementID=$(this).parent().find($(" input:visible:visible")).attr("id");
+			   if(attrElementID.includes('DateTimePicker')){
+				   returnStepsCode=returnStepsCode + ".input(DateTime.addToCurrentDate(noOfDays), Fields." + newLabelValue + ")\n";
+			   	   //console.log(".input(DateTime.addToCurrentDate(noOfDays), Fields." + newLabelValue + ")");
+			   }
+			   if(attrElementID.includes('BluePagesLookupField')){
+				   returnStepsCode=returnStepsCode + ".input(DateTime.addToCurrentDate(noOfDays), Fields." + newLabelValue + ")\n";
+			   	   //console.log(".input(\"BULHAC, MIHAI (Mihai)\", Fields." + newLabelValue + ")");
+			   }
+			   if(attrElementID.includes('InputField')){
+				   returnStepsCode=returnStepsCode + ".input(\"test\", Fields." + newLabelValue + ")\n";
+			   	   //console.log(".input(\"test\", Fields." + newLabelValue + ")");
+			   }
+		   }
+		   if ($(this).parent().find($(" textarea:visible")).length>0){
+			   returnStepsCode=returnStepsCode + ".textArea(\"test\", Fields." + newLabelValue + ")\n";
+			   //console.log(".textArea(\"test\", Fields." + newLabelValue + ")" );
+		   }
+		   if ($(this).parent().find($(" select:visible")).length>0){
+			   returnStepsCode=returnStepsCode + ".multiSelect(multiSelectValue, Fields." + newLabelValue + ")\n";
+			   //console.log(".multiSelect(multiSelectValue, Fields." + newLabelValue + ")" );
+		   }
+		});
+		return  returnStepsCode;
+    }
+});
+	
 });
